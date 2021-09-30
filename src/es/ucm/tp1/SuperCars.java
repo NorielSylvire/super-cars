cf48a24d5d93c3a21a35104edc8541c9b3c3f251
Flaviu E. Hongu <39311030+Fl4v1u5@users.noreply.github.com>
2021-09-30 12:11:23 +0200

    abueli

--- src/es/ucm/tp1/SuperCars.java
+++ src/es/ucm/tp1/SuperCars.java
@@ -6,7 +6,7 @@ import es.ucm.tp1.control.Controller;
 import es.ucm.tp1.control.Level;
 import es.ucm.tp1.logic.Game;
 
-/* Holi panoli*/
+/* Holi panoli; Panoli mi abueli*/
 
 public class SuperCars {
 
