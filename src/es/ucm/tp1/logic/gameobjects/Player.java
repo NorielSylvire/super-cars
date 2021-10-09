770f764e2c2b442e8469933213e7e9cc755d3487
Hachendado <byquimic@gmail.com>
2021-10-09 10:57:41 +0200

    Primeros cambios al Player
    Up,down, update simples

--- src/es/ucm/tp1/logic/gameobjects/Player.java
+++ src/es/ucm/tp1/logic/gameobjects/Player.java
@@ -1,6 +1,32 @@
 package es.ucm.tp1.logic.gameobjects;
 
+import es.ucm.tp1.logic.Game;
+
 public class Player {
 	private int posX;
 	private int posY;
+	
+	
+	private int monedas;
+	
+	public void PlayerUp(Game partida, Player player) {
+		
+	
+		player.posY--;
+		
+		return;
+	}
+	public void PlayerDown(Game partida, Player player) {
+	
+		player.posY++;
+		
+		return;
+	}
+	
+	public void Upadte(Game partida,Player player) {
+	
+		player.posX++;
+		
+	}
+	
 }
