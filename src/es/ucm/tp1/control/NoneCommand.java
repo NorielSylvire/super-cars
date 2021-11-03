3e2e740dbfcfc7fe8d08e83d256d0835b123ab1b
Hachendado <byquimic@gmail.com>
2021-11-03 14:36:41 +0100

    Pirirpi

---
+++ src/es/ucm/tp1/control/NoneCommand.java
@@ -0,0 +1,13 @@
+package es.ucm.tp1.control;
+
+import es.ucm.tp1.logic.Game;
+
+public class NoneCommand extends Command {
+
+	@Override
+	public boolean execute(Game game) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
--- src/es/ucm/tp1/logic/lists/GameObjectList.java
+++ src/es/ucm/tp1/logic/lists/GameObjectList.java
@@ -4,13 +4,12 @@ import es.ucm.tp1.logic.gameobjects.*;
 import es.ucm.tp1.control.Level;
 import java.util.*;
 
-//ESTO PARA LUEGOOOOO
 
 public class GameObjectList {
-	//private ArrayList<GameObject> gameObjects;
+	private ArrayList<GameObject> gameObjects;
 	private int numObjects;
 	
-	/*public GameObjectList(Level l) {
+	public GameObjectList() {
 		this.gameObjects = new ArrayList<GameObject>();
 		this.numObjects = 0;
 		
@@ -19,13 +18,10 @@ public class GameObjectList {
 	public void add(GameObject obj) {
 		gameObjects.add(obj);
 		numObjects++;
-	}*/
-	
-	public GameObjectList() {
-		this.numObjects = 0;
 	}
 	
 	public int getNumObjects() {
 		return this.numObjects;
 	}
+	
 }
